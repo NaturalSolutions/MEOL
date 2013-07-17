4181643f80b27dcd8ca5e63e77fcbe3fad2cacd5
cdcvidal <cdcvidal@yahoo.fr>
2013-07-17 17:55:04 +0200

    minification

--- Meol-app/www/js/view.js
+++ Meol-app/www/js/view.js
@@ -963,7 +963,7 @@ directory.views.TableProfilDetailView =  directory.views.BaseView.extend({
  destroyTScore: function(event){
     this.collection.destroy();
     this.scoreTotal = 0;
-    this.lastActiveGalleryName = directory.data.galleriesList.findWhere({'ordre' : 1}).get('name');;
+    this.lastActiveGalleryName = directory.data.galleriesList.findWhere({'ordre' : 1}).get('name');
  },
  showModalReset: function(event){
   $("#resetModal").modal('show');
